8554737ce0b4348671764addc0e0a77e483865c0
XealRetro <megalodonxgaming@gmail.com>
2017-10-25 19:05:17 -0400

    Test

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -1,1 +1,26 @@
+import sys
+keys = range(10)
+story_list = {}
+word_list = {}
 
+conv_option = str.strip(sys.argv[1])
+with open(str.strip(sys.argv[2])) as d:
+     word_list = d.read().splitlines()
+with open(str.strip(sys.argv[3])) as f:
+     story_list = f.read().splitlines()
+
+
+if conv_option == "1":
+    for i in keys:
+        print(story_list[i])
+        print(word_list[i].upper())
+elif conv_option == "2":
+    for i in keys:
+        print(story_list[i])
+        print(word_list[i].lower())
+elif conv_option == "3":
+    for i in keys:
+        print(story_list[i])
+        print(word_list[i].title())
+
+       
